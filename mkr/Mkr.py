b0b926dcae3b308e1fd815b15a2836936ffd6963
tramon <andriitramon@gmail.com>
2024-12-16 09:01:59 +0200

    mkr

---
+++ mkr/Mkr.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+class Mkr:
+
+    @staticmethod
+    def calculate_integrated_score(data):
+        # Нормалізація ціни (чим менше, тим краще)
+        data['price_norm'] = data['price'].min() / data['price']
+
+        # Нормалізація продуктивності
+        performance_features = ['ram', 'ssd', 'gpu_score', 'refresh_rate']
+        for feature in performance_features:
+            data[f'{feature}_norm'] = data[feature] / data[feature].max()
+        data['performance_norm'] = data[[f'{feature}_norm' for feature in performance_features]].mean(axis=1)
+
+        # Нормалізація тривалості батареї (battery life)
+        data['battery_norm'] = data['battery_life'] / data['battery_life'].max()
+
+        # Інтегрована оцінка
+        data['integrated_score'] = data[['price_norm', 'performance_norm', 'battery_norm']].mean(axis=1)
+        top_10_laptops = laptops.sort_values(by='integrated_score', ascending=False).head(10)
+        return top_10_laptops
+
+    @staticmethod
+    def plot(data):
+        plt.bar(data["model"], data["integrated_score"])
+        plt.title("Інтегрована оцінка ноутбуків")
+        plt.xlabel("Назва товару")
+        plt.ylabel("Оцінка")
+        plt.show()
+
+if __name__ == '__main__':
+    laptops = pd.read_csv("laptops.csv")
+
+    top_10_laptops = Mkr.calculate_integrated_score(laptops)
+    Mkr.plot(top_10_laptops)
+
